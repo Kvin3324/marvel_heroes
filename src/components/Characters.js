53a6a049bee58f307b7c882d8af5ef4259012873
Kévin Joya <kevin.joya@outlook.com>
2019-09-23 21:04:21 +0200

    Refacto code for search bar and render

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,11 +1,17 @@
 import React from "react"
 import HeroCards from "./HeroCards"
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import AboutHero from "./AboutHero";
 
 class Characters extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      characters: null
+      characters: null,
+      showList: {
+        list: true,
+        searchInput: ""
+      }
     }
   }
 
@@ -13,26 +19,70 @@ class Characters extends React.Component {
     const url = "https://gateway.marvel.com/"
     const apiKey = "f5d9657d8bb68f805b1ec30fe13cf70d"
 
-    fetch(`${url}v1/public/characters?apikey=${apiKey}`)
+    fetch(`${url}v1/public/characters?apikey=${apiKey}&limit=40`)
       .then(response => response.json())
       .then(data => {
-        console.log(data.data.results);
+        console.log(data.data.results)
         this.setState({
         characters: data.data.results
       })
     })
+  }
+
+  handleOnChange = (event) => {
+    if (event.target.value.length !== "") {
+      this.setState({
+        showList: {
+          list: false,
+          searchInput: event.target.value
+        }
+      })
+    } else {
+      this.setState({
+        showList: {
+          list: true,
+          searchInput: ""
+        }
+      })
     }
+  }
 
   render() {
     return (
-      <div className="row">
-        {this.state.characters === null ?
-          "loading...":
-          this.state.characters.map(character => {
-            return <HeroCards name={character.name} description={character.description} img_hero={character.thumbnail}  />
-          })
-      }
-      </div>
+      // <Router>
+        <React.Fragment>
+          <div className="search--bar">
+            <input
+            className="form-control search--bar--input"
+            type="text"
+            placeholder="Search"
+            aria-label="Search"
+            onChange={e => this.handleOnChange(e) }
+            />
+          </div>
+          <div className="row">
+            { (function() {
+              if(this.state.characters === null) {
+                return "loading..."
+              }
+              if(this.state.characters !== null) {
+                if (this.state.showList.list) {
+                  return this.state.characters.map(character => {
+                    return <HeroCards name={character.name} id={character.id} description={character.description} img_hero={character.thumbnail} key={character.name} />
+                  })
+                }
+                if (!this.state.showList.list) {
+                  return this.state.characters.map(character => {
+                    return character.name.includes(this.state.showList.searchInput) ? <HeroCards name={character.name} id={character.id} description={character.description} img_hero={character.thumbnail} key={character.name} /> : null
+                  })
+                }
+              }
+            }).bind(this) ()
+             }
+          {/* <Route path="/about" component={AboutHero} /> */}
+          </div>
+        </React.Fragment>
+    //  </Router>
     )}
 }
 
