aada982015b636398735160e61c5cac5b5fc17d4
Kévin Joya <kevin.joya@outlook.com>
2019-09-23 21:05:07 +0200

    Added route link and some infos about character

--- src/components/HeroCards.js
+++ src/components/HeroCards.js
@@ -1,18 +1,27 @@
 import React from "react"
+import {Link} from "react-router-dom";
+// import AboutHero from "./AboutHero"
+
 
 function HeroCards(props) {
-  console.log(props);
   return (
-        <div className="col-xl-4">
-          <div className="card">
-            <img src={`${props.img_hero.path}.${props.img_hero.extension}`} className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title"> {props.name} </h5>
-              <p className="card-text">{props.description}</p>
-              <button className="btn btn-primary">Go to desc</button>
-            </div>
-          </div>
+    <div className="col-xl-4">
+      <div className="card">
+        <img src={`${props.img_hero.path}.${props.img_hero.extension}`} className="card-img-top" alt="..."/>
+        <div className="card-body">
+          <h5 className="card-title"> {props.name}</h5>
+          <p>{props.id}</p>
+          <p className="card-text">{props.description}</p>
+          <button className="btn btn-secondary">
+            <Link to={`/about/${props.id}`}>Goooo</Link>
+          </button>
+          {/* <Link to="/AboutHero"> */}
+            {/* <button className="btn btn-primary">Go to desc</button> */}
+            {/* Go */}
+          {/* </Link> */}
         </div>
+      </div>
+    </div>
     )
 }
 
