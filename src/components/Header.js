959e0f98637a4d71b2927b023b6c2c71480d719c
Kévin Joya <kevin.joya@outlook.com>
2019-09-24 15:54:41 +0200

    Added navlink home and style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
-        <header className="title">Marvel characters</header>
+      <header className="title">
+        <div className="d-flex ml-3 pt-3 header--home">
+          <NavLink to="/">Home</NavLink>
+        </div>
+        Marvel Characters
+      </header>
     )
 }
 
