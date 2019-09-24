24ebab41485326400a0ca1a52494066d515af172
Kévin Joya <kevin.joya@outlook.com>
2019-09-24 15:56:27 +0200

    Added render compo SingleHero

--- src/components/AboutHero.js
+++ src/components/AboutHero.js
@@ -1,10 +1,10 @@
 import React, {useEffect, useState} from "react";
+import SingleHero from './SingleHero';
 
 const AboutHero = props => {
   const [state, setState] = useState({
     characters: null
   });
-
 
   useEffect(() => {
     const url = "https://gateway.marvel.com/"
@@ -13,11 +13,22 @@ const AboutHero = props => {
     fetch(`${url}v1/public/characters/${props.match.params.id}?apikey=${apiKey}`)
       .then(response => response.json())
       .then(data => setState({characters: data.data.results}))
-  }, []);
+    }, []);
+    console.log(state);
 
   return (
     <div>
-      {state.characters === null ? "loading" : state.characters[0].name}
+      {state.characters === null ?
+        "loading" :
+        < SingleHero
+          name={state.characters[0].name}
+          id={state.characters[0].id}
+          description={state.characters[0].description}
+          img_hero={state.characters[0].thumbnail}
+          comics={state.characters[0].comics}
+          key={state.characters[0].id}
+        />
+      }
     </div>
   );
 };
