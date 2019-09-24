28972b5bfd57f8e4329eb3654fefdfdda0196b69
Kévin Joya <kevin.joya@outlook.com>
2019-09-24 15:56:48 +0200

    Created render about hero

---
+++ src/components/SingleHero.js
@@ -0,0 +1,29 @@
+import React from "react"
+
+function HeroCards(props) {
+  return (
+    <div className="col-xl-12 about--hero">
+      <div className="col-xl-6 about--hero--img">
+        <img src={`${props.img_hero.path}.${props.img_hero.extension}`} className="card-img-top" alt="..."/>
+      </div>
+        <div className="col-xl-6 about--hero--infos">
+          <h5 className="about--hero--name"> {props.name}</h5>
+          <p className="about--hero--description">{props.description}</p>
+          <strong>His comics:</strong>
+          {
+            props.comics.items.map((item, index) => {
+              return (
+              <p className="pt-3">
+                {props.comics.items[index].name}
+                <br/>
+                {props.comics.items[index].resourceURI}
+              </p>
+              )
+            })
+          }
+      </div>
+    </div>
+  )
+}
+
+export default HeroCards
