9b770c4933a708645f5b0946f09c6af27c2812b7
Kévin Joya <kevin.joya@outlook.com>
2019-09-25 18:25:53 +0200

    Added id

--- src/components/HeroCards.js
+++ src/components/HeroCards.js
@@ -8,7 +8,7 @@ function HeroCards(props) {
         <img src={`${props.img_hero.path}.${props.img_hero.extension}`} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title"> {props.name}</h5>
-          <p>{props.id}</p>
+          <p>(#{props.id})</p>
           <p className="card-text">{props.description}</p>
           <button className="btn btn-secondary">
             <Link to={`/about/${props.id}`} className="link--to--go">Goooo</Link>
