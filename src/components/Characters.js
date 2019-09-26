bd730cd9f97066084dd5b86e732c9b0b22710940
Kévin Joya <kevin.joya@outlook.com>
2019-09-26 15:12:50 +0200

    Added container

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -22,6 +22,7 @@ class Characters extends React.Component {
     fetch(`${this.state.url}v1/public/characters?apikey=${this.state.apiKey}&limit=10&offset=${page * 10}`)
     .then(response => response.json())
     .then(data => {
+      console.log(data);
       this.setState({
         characters: data.data.results
       })
@@ -68,28 +69,30 @@ class Characters extends React.Component {
           />
           <p className="search--bar--input--hint">(Don't forget: a name begins with an uppercase. <span role="img" aria-label="">😉</span>)</p>
         </div>
-        <div className="pagination">
-          <Pagination onChange={page => this.getList(page)} defaultPageSize={10} total={1000} pageSize={10}  />
-        </div>
-        <div className="row">
-          { (function() {
-            if(this.state.characters === null) {
-              return <i className="fa fa-spinner"></i>
-            }
-            if(this.state.characters !== null) {
-              if (this.state.showList.list) {
-                return this.state.characters.map(character => {
-                  return <HeroCards name={character.name} id={character.id} description={character.description} img_hero={character.thumbnail} key={character.name} />
-                })
+        <div className="container container--cards">
+          <div className="pagination">
+            <Pagination onChange={page => this.getList(page)} defaultPageSize={10} total={1000} pageSize={10}  />
+          </div>
+          <div className="row">
+            { (function() {
+              if(this.state.characters === null) {
+                return <i className="fa fa-spinner"></i>
               }
-              if (!this.state.showList.list) {
-                return this.state.characters.map(character => {
-                  return character.name.includes(this.state.showList.searchInput) ? <HeroCards name={character.name} id={character.id} description={character.description} img_hero={character.thumbnail} key={character.id} /> : null
-                })
+              if(this.state.characters !== null) {
+                if (this.state.showList.list) {
+                  return this.state.characters.map(character => {
+                    return <HeroCards name={character.name} id={character.id} description={character.description} img_hero={character.thumbnail} key={character.name} />
+                  })
+                }
+                if (!this.state.showList.list) {
+                  return this.state.characters.map(character => {
+                    return character.name.includes(this.state.showList.searchInput) ? <HeroCards name={character.name} id={character.id} description={character.description} img_hero={character.thumbnail} key={character.id} /> : null
+                  })
+                }
               }
-            }
-          }).bind(this) ()
+            }).bind(this) ()
           }
+          </div>
         </div>
       </React.Fragment>
     )}
