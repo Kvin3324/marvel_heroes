03eac51efecb3564171cf14fe6a4ddde88a1ab88
Kévin Joya <kevin.joya@outlook.com>
2019-09-26 16:36:23 +0200

    Added logo and changed home link

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import {NavLink} from "react-router-dom";
 function Header() {
     return (
       <header className="title">
-        <div className="d-flex ml-3 pt-3 header--home">
+        <div className="pt-4 header--home">
           <NavLink to="/" className="header--text">Home</NavLink>
         </div>
-        Marvel Characters
+        <img src="https://upload.wikimedia.org/wikipedia/fr/a/a4/Logo_Marvel_Comics.jpg" alt="marvel-logo" className="logo-marvel"></img>
       </header>
     )
 }
