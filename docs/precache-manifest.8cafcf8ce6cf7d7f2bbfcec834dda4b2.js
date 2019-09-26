34162ed80d4f79c3dbd77bc85bb374c5c5ea1c90
Kévin Joya <kevin.joya@outlook.com>
2019-09-26 16:42:55 +0200

    Added folder to deploy

---
+++ docs/precache-manifest.8cafcf8ce6cf7d7f2bbfcec834dda4b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eafbb6d246a2effbfd8825f476bffaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fae42f087a21c9188eb",
+    "url": "/static/css/2.7fe71732.chunk.css"
+  },
+  {
+    "revision": "521f9b38facad8190a08",
+    "url": "/static/css/main.d534992d.chunk.css"
+  },
+  {
+    "revision": "8fae42f087a21c9188eb",
+    "url": "/static/js/2.837a5b0e.chunk.js"
+  },
+  {
+    "revision": "521f9b38facad8190a08",
+    "url": "/static/js/main.f141e1f9.chunk.js"
+  },
+  {
+    "revision": "d122027b965853605117",
+    "url": "/static/js/runtime~main.b49bb5cb.js"
+  }
+]);
