62b567f3b678aeeeb672ddcec90be0f86f7817cf
Kévin Joya <kevin.joya@outlook.com>
2019-09-26 15:13:51 +0200

    Changed name class

--- src/components/SingleHero.js
+++ src/components/SingleHero.js
@@ -4,12 +4,12 @@ function HeroCards(props) {
   return (
     <div className="col-xl-12 mb-3 about--hero">
       <div className="col-xl-6 about--hero--img">
-        <img src={`${props.img_hero.path}.${props.img_hero.extension}`} className="card-img-top" alt="..."/>
+        <img src={`${props.img_hero.path}.${props.img_hero.extension}`} className="card--img--single" alt="..."/>
       </div>
         <div className="col-xl-6 about--hero--infos">
           <h5 className="about--hero--name"> {props.name}</h5>
           <p className="about--hero--description">{props.description}</p>
-          <strong>His comics:</strong>
+          <strong>Comics:</strong>
           {
             props.comics.items.map((item, index) => {
               return (
